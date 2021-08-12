ca55b65d622375997bfb5f5cf882657280bbf8d6
kykwhz <kykwhz@gmail.com>
2021-08-12 12:01:56 +0900

    click_num()を実装

--- js/default.js
+++ js/default.js
@@ -1,2 +1,25 @@
 // JavaScriptファイルが読み込まれているか確認
 // alert('test');
+
+function click_num(num){
+    a = document.getElementById("disp_input");
+    i = a.innerText;
+    num = parseInt(num);
+    if(!(a == 0 && num == 0)){
+        i = i * 10 + num;
+        a.innerText = i;
+    }
+}
+
+function click_C(){
+    a = document.getElementById("disp_input");
+    b = document.getElementById("disp_output");
+    a.innerText = 0;
+    b.innerText = "";
+}
+
+function click_plus(){
+    a = document.getElementById("disp_input");
+    b = document.getElementById("disp_output");
+    b.innerText = a.innerText + "+";
+}
